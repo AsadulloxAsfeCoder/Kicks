88e7c380fb2130efbdfda4f532da2af3c2b8c8f6
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2025-06-17 09:50:57 +0500

    kicks

--- app/landing/product/[slug]/page.tsx
+++ app/landing/product/[slug]/page.tsx
@@ -5,19 +5,22 @@ import Siteheader from "@/components/common/site-header"
 import Footer from "@/components/ui/footer"
 import ProductDetal from "@/components/listing/product-detal"
 import ReledetProduct from "@/components/listing/releted-product"
-type Props = {
+
+interface PageProps {
   params: {
     slug: string
   }
 }
 
-const ProductDetailPage = ({ params: { slug } }: Props) => {
+export default function ProductDetailPage({ params }: PageProps) {
+  const { slug } = params
+
   return (
     <main>
       <Siteheader />
       <section className="py-8">
         <div className="container mx-auto">
-        <ProductDetal params={{ slug }} />
+          <ProductDetal params={{ slug }} />
         </div>
       </section>
       <section>
@@ -25,7 +28,5 @@ const ProductDetailPage = ({ params: { slug } }: Props) => {
       </section>
       <Footer />
     </main>
-  ) 
+  )
 }
-
-export default ProductDetailPage
